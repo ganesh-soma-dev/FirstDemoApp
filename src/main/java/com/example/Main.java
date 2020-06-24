4fe3a31ab5b29bdc696451b78f51a269ab4403c2
Ganesh Soma <gsoma@salesforce.com>
2020-06-24 15:40:13 +0530

    Added db file

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -40,4 +40,54 @@ public class Main {
   String index() {
     return "index";
   }
+  
+  
+  @RequestMapping("/confdemo")
+	String hello(Map<String, Object> model) {
+		RelativisticModel.select();
+		String energy = System.getenv().get("ENERGY");
+		if (energy == null) {
+			energy = "12 GeV";
+		}
+		Amount<Mass> m = Amount.valueOf(energy).to(KILOGRAM);
+		// model.put("science", "E=mc^2: 12 GeV = " + m.toString());
+		model.put("science", "E=mc^2: " + energy + " = " + m.toString());
+		return "confdemo";
+	}
+  
+  @Autowired
+  private DataSource dataSource;
+
+@RequestMapping("/db")
+  String db(Map<String, Object> model) {
+    try (Connection connection = dataSource.getConnection()) {
+      Statement stmt = connection.createStatement();
+      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+      ArrayList<String> output = new ArrayList<String>();
+      while (rs.next()) {
+        output.add("Read from DB: " + rs.getTimestamp("tick"));
+      }
+
+      model.put("records", output);
+      return "db";
+    } catch (Exception e) {
+      model.put("message", e.getMessage());
+      return "error";
+    }
+  }
+
+  @Bean
+  public DataSource dataSource() throws SQLException {
+    if (dbUrl == null || dbUrl.isEmpty()) {
+      return new HikariDataSource();
+    } else {
+      HikariConfig config = new HikariConfig();
+      config.setJdbcUrl(dbUrl);
+      return new HikariDataSource(config);
+    }
+  }
+  
 }
